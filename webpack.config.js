04382bf3337a84069d9ae8ed64f6c07af3f37a6f
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-02 18:57:27 -0700

    Feat: Add HtmlWebpackPlugin.

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,13 @@ module.exports = {
         filename:'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Ping Pong',
+            template: './src/index.html',
+            inject: 'body'
+        })
+    ],
     module: {
         rules: [
             {
